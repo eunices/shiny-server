1f2a796138b9e3f4d94ad7c9dfc699921f83371f
Eunice Soh <eunice.sjy@gmail.com>
2020-03-13 14:58:30 +0800

    [EJYS] Test

--- apps/eda1.1_shiny/app.r
+++ apps/eda1.1_shiny/app.r
@@ -71,8 +71,43 @@ update <- function(threshold=DEFAULT_THRESHOLD, country=NA, typeInput=c()) {
 
 }
 
+
+# UI component of shiny app
+ui <- fluidPage(
+  verticalLayout(
+    inputPanel(
+      sliderInput("thresholdInput", "Minimum threshold", 
+                  min=0, max=floor(sort(t$N)[length(t$N)-1]/10)*10, 
+                  value=DEFAULT_THRESHOLD, post=" N spp.",
+                  step=5),
+      selectInput("nodeColourInput", "Node colour by",
+                  c("Country", "Colonialism", "Economic status"), selected=NULL, multiple=F),
+      selectInput("countryInput", "Country", # sorted by Discover Life codes
+                  c("All", nodes_lab), selected=NULL, multiple=F),
+      conditionalPanel(
+        condition = "input.countryInput != 'All'",
+        checkboxGroupInput("typeInput", "Type (Source or Target)", 
+                          choices = list("Source" = 1, "Target" = 2),
+                          selected = c(1, 2))
+      )
+    ),
+    mainPanel(
+      sidebarLayout(
+        sidebarPanel(tableOutput("networkTable"),
+                     "Legend:", 
+                     "Src = Source i.e., country of taxonomist", 
+                     "Tgt = Target i.e. country of type species", 
+                     "N = N species described",
+                     width=2), 
+        mainPanel(forceNetworkOutput("network", height="500px"), width=10)
+      ),
+      width=12
+    )
+  )
+)
+
 # Server component of shiny app
-shinyServer(function(input, output) {
+server <- function(input, output) {
 
   # Update data
   update_data <- reactive({
@@ -125,4 +160,7 @@ shinyServer(function(input, output) {
 
   })
 
-})
+}
+
+# Run app
+shinyApp(ui = ui, server = server)
